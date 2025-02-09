02abe082cf659085e3a4e21c2b47321d228821cd
Emilio Santos Mendonca <T30689@grupoaval.com.br>
2025-02-09 20:40:59 -0300

    criando metodo put

--- backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
+++ backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,12 @@ public class UsuarioController{
 
     @PostMapping
     public ResponseEntity<Usuario> criarUsuarios(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return ResponseEntity.status(200).body(usuarioNovo);
+    }
+    
+    @PutMapping
+    public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
         Usuario usuarioNovo = dao.save(usuario);
         return ResponseEntity.status(200).body(usuarioNovo);
     }
