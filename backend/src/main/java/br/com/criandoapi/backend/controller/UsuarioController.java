0305dc4afa2c66168726346252555909e13a8b65
Emilio Santos Mendonca <T30689@grupoaval.com.br>
2025-02-09 20:46:57 -0300

    criando metodo de delete do usuário

--- backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
+++ backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
@@ -1,10 +1,13 @@
 package br.com.criandoapi.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,5 +43,10 @@ public class UsuarioController{
         Usuario usuarioNovo = dao.save(usuario);
         return ResponseEntity.status(200).body(usuarioNovo);
     }
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Optional<Usuario>> excluirUsuario(@PathVariable Integer id){
+		Optional<Usuario> deleteUsuario = dao.findById(id);
+		dao.deleteById(id);
+        return ResponseEntity.status(200).body(deleteUsuario);
+	}
 }
