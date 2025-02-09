992df6e7dfd57de1e64d32c2a0a7e84aa90017c3
Emilio Santos Mendonca <T30689@grupoaval.com.br>
2025-02-09 20:36:27 -0300

    adicionado o swagger para tratamento de rotas, e configurando a requisição POSTmapping

--- backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
+++ backend/src/main/java/br/com/criandoapi/backend/controller/UsuarioController.java
@@ -3,21 +3,35 @@ package br.com.criandoapi.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoapi.backend.DAO.IUsuario;
 import br.com.criandoapi.backend.model.Usuario;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController{
 	
 	@Autowired
 	private IUsuario dao;
 	
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> listaUsuarios() {
 		return (List<Usuario>) dao.findAll();
 	}
+
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuarios(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return ResponseEntity.status(200).body(usuarioNovo);
+    }
+	
 }
